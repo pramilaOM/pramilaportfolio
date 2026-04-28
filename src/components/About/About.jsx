09b022c1c6ff2e99e813d9f4860631dcd6d76942
Pramila Yadav <pramilayadavomswami@gmail.com>
2026-04-28 12:20:04 -0400

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -47,9 +47,8 @@ const About = () => {
           </p>
 
           {/* Resume Button */}
-          <a
-           // href="https://drive.google.com/file/d/1wMS4dKnlrnbKtK4Hp5viohnexo8A1wqn/view?usp=sharing"
-             https://drive.google.com/uc?export=download&id=1VkZoAHPGFOroPjtLRu0WBH0lL6QpoEVl
+         <a
+            href="https://drive.google.com/uc?export=download&id=1VkZoAHPGFOroPjtLRu0WBH0lL6QpoEVl"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
